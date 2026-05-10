7c0ac93b203e580ec39133955d7447ff55a1fc22
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-05-10 10:47:37 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/cspringit1/Cspringit1Application.java
@@ -0,0 +1,13 @@
+package com.example.cspringit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cspringit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cspringit1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cspringit1/Cspringit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cspringit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cspringit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
